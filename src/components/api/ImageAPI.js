c5219ac96af9e25db2868dd18edd35aad03362c1
Artem Horiuk <62579471+artemhoriuk1994@users.noreply.github.com>
2022-08-30 17:51:31 +0300

    changes

--- src/components/api/ImageAPI.js
+++ src/components/api/ImageAPI.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
-const KEY = '28301212-4ad2581bf94573ba1d8a79e90';
+const KEY = '28394291-ce42361a1eeba82eae067226a';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-
-
-
 
 export async function fetchImages(name, page, perPage) {
   return await axios
     .get(
       `?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
     )
-      .then(response => response.data);
+    .then(response => response.data);
 }
